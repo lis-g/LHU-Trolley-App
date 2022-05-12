b346691bc615e261b724e0a03b48321217274e7d
eg <eg145341@gmail.com>
2022-05-11 23:18:13 -0400

    Add files via upload

---
+++ LHU-Trolley-App-main/java/com/comp475/lhutrolleyapp/activity/EditNotificationActivity.java
@@ -0,0 +1,16 @@
+package com.comp475.lhutrolleyapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.comp475.lhutrolleyapp.R;
+
+public class EditNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_notification);
+    }
+}
